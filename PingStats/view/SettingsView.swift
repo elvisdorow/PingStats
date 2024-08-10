4ee669760f054c4ddaabfdf81f2842215fcb5996
Elvis Dorow <edorow@gmail.com>
2024-08-09 21:27:49 -0300

    .

---
+++ PingStats/view/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  PingStats
+//
+//  Created by Elvis Dorow on 09/08/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
